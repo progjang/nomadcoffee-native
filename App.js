eb1c178dc450f255ef11eba5f7a05e2968b8c089
PJ <wooritobi@gmail.com>
2021-12-20 23:10:11 +0900

    Preloading fonts, images and Ionicons

--- App.js
+++ App.js
@@ -1,7 +1,26 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Asset} from 'expo-asset';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+
+  const preload = () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map(font=> Font.loadAsync(font));
+    console.log(fontPromises);
+    const imagesToLoad = [require("./assets/coffee-logo.png")];
+    const imagePromises = imagesToLoad.map(image=> Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
+  }
+  if(loading){
+    return <AppLoading onError={console.warn} onFinish={onFinish} startAsync={preload} />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
